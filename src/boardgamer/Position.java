4548b1b9a9cc09844b68931988f8715274c00e12
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2023-01-14 02:08:40 -0300

    First class: Position

---
+++ src/boardgamer/Position.java
@@ -0,0 +1,36 @@
+package boardgamer;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int colunm) {
+		this.row = row;
+		this.column = colunm;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColunm() {
+		return column;
+	}
+
+	public void setColunm(int colunm) {
+		this.column = colunm;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	public void setValues(int row, int column) {
+	}
+}
